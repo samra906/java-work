bf1395b470a91018f6cba2b8c849759da2650528
samra906 <samramaryam2001@gmail.com>
2023-01-30 23:11:48 +0500

    pattern

---
+++ first-programs/pattern.java
@@ -0,0 +1,12 @@
+public class pattern {
+    
+    public static void name(String[] args) {
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+        System.out.println("*****");
+    }
+    
+    
+}
